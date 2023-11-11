ef6bf4a2da2680acf6e520ed84a11a5f9313e60d
Komodorpudel <sah_services@outlook.com>
2023-11-11 22:30:51 +0100

    sasd

---
+++ C_BP/bp02_SUB/Aufgabe_2.1/Aufgabe_2_1b/A_2_1b.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+int main() {
+    
+    // Zufallszahl generieren und speichern
+    srand((unsigned int)time(NULL));
+
+    char c = rand() % 128;
+
+    printf("random character: %c\n", c);
+
+    if (c <= 'a' || c > 'z') {
+
+        printf("Der Char %c ist kein lateinischer Kleinbuchstabe", c);
+    }
+
+    return 0;
+
+}
---
+++ C_BP/bp02_SUB/Aufgabe_2.5/Aufgabe_2_5a/A_2_5a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+#include <math.h>
+
+int main() {
+    
+    // Zufallszahl generieren und speichern
+    int myNumber;
+    int index;
+    double zwischen;
+
+    srand((unsigned int)time(NULL));
+
+    //int myNumber = rand() % 10;
+    myNumber = 8;
+
+    index = 1;
+
+    // loopen
+    while(index <= myNumber) {
+        zwischen = pow(index, 0.5);
+        printf("%.3e       ", zwischen);
+        ++index;
+    }
+
+    return 0;
+
+}
