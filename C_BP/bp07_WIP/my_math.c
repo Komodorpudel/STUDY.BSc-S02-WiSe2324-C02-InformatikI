664ff35bea703ba7cbb58bb436bfdfd3c0f32528
Komodorpudel <sah_services@outlook.com>
2023-12-12 18:25:42 +0100

    sad

---
+++ C_BP/bp07_WIP/my_math.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "my_math.c"
+
+double my_pow(double x, int y) {
+
+
+    return ;
+}
---
+++ C_BP/bp07_WIP/my_math.h
@@ -0,0 +1,13 @@
+#ifndef my_math_h
+
+/* Anfang */
+#define my_math_h
+
+/* Makro */
+#define MY_FABS(x) ((x) > 0 ? (x) : -(x))              
+
+/* Funktionsprototypen */
+double my_pow(double x, int y);
+
+#endif
+/* Ende */
