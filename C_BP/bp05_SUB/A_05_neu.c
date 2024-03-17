c7cb45e65a15a6f000d23854584270fcb3619888
Komodorpudel <sah_services@outlook.com>
2024-03-17 19:33:35 +0100

    sad

--- C_BP/bp05_SUB/A_05_neu.c
+++ C_BP/bp05_SUB/A_05_neu.c
@@ -6,14 +6,57 @@ int main(void){
 
 }
 
-unsigned int my_interval_rand(unsigned int u; unsigned int o)
+unsigned int my_interval_rand(unsigned int u, unsigned int o)
 {
-    int zahlenfeld[10];
     int i;
+    unsigned int min;
+    unsigned int max;
 
     srand(time(NULL));
 
-    for(i = 0; i < 10; i++){
-
+    /* Min und Max finden */
+    if (u > o){
+        max = u;
+        min = o;
     }
+    else {
+        max = o;
+        min = u;
+    }
+
+    /* Zufallszahl generieren und zurückgeben*/
+    return (min + rand() % ((max - min) + 1));
+
+}
+
+void array_init(unsigned int w[], unsigned int n, unsigned int u, unsigned int o)
+{
+    int i;
+    for(i = 0; i < n; i++){
+        w[i] = my_interval_rand(u, o);
+    }
+}
+
+unsigned int array_second_min(unsigned int w[], unsigned int n)
+{
+    unsigned int first_min = UNIT_MAX;
+    unsigned int second_min = UNIT_MAX;
+    int i;
+
+    if (n < 2){
+        return UNIT_MAX;
+    }
+
+    for(i = 0; i < n; i++) {
+        if (w[i] <= first_min){
+            second_min = first_min;
+            first_min = w[i];
+        }
+        else if(w[i] < second_min){
+            second_min = w[i];
+        }
+    }
+
+    return second_min;
+
 }
