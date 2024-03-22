41b87d90f232f67fb86167e6dcf0050bcda0e04a
Komodorpudel <sah_services@outlook.com>
2024-03-22 10:16:22 +0100

    sad

--- C_BP/bp07_SUB/A_7_neu/my_math.h
+++ C_BP/bp07_SUB/A_7_neu/my_math.h
@@ -1,8 +1,8 @@
 #ifndef MY_MATH_H_INCLUDED
 #define MY_MATH_H_INCLUDED
 
-#define MY_FABS(X) ((x) < 0 ? -(x) : (x))
+#define MY_FABS(X) ((X) < 0 ? -(X) : (X))
 
-my_pow(double x, int y);
+double my_pow(double x, int y);
 
 #endif
